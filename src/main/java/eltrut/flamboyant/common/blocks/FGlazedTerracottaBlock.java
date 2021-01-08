2d9c52fca96b048bf29814be8bb710cad8f8ef05
EltrutLikes <10eltrut1@gmail.com>
2021-01-07 22:05:04 -0500

    fix crash i somehow missed

--- src/main/java/eltrut/flamboyant/common/blocks/FGlazedTerracottaBlock.java
+++ src/main/java/eltrut/flamboyant/common/blocks/FGlazedTerracottaBlock.java
@@ -2,38 +2,19 @@ package eltrut.flamboyant.common.blocks;
 
 import com.minecraftabnormals.abnormals_core.core.util.item.filling.TargetedItemGroupFiller;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFaceBlock;
-import net.minecraft.block.material.PushReaction;
-import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.block.GlazedTerracottaBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.state.StateContainer;
 import net.minecraft.util.NonNullList;
 
-public class FGlazedTerracottaBlock extends HorizontalFaceBlock {
+public class FGlazedTerracottaBlock extends GlazedTerracottaBlock {
 	private static final TargetedItemGroupFiller FILLER = new TargetedItemGroupFiller(() -> Items.BLACK_GLAZED_TERRACOTTA);
 
 	public FGlazedTerracottaBlock(Properties properties) {
 		super(properties);
 	}
 
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(HORIZONTAL_FACING);
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
-	}
-
-	@Override
-	public PushReaction getPushReaction(BlockState state) {
-		return PushReaction.PUSH_ONLY;
-	}
-	
 	@Override
 	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
 		FILLER.fillItem(this.asItem(), group, items);
