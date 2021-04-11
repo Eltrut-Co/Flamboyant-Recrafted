230eb162383603629bd65a3b2850b553ef227a09
EltrutLikes <10eltrut1@gmail.com>
2021-04-11 12:48:58 -0400

    make villagers sleep in flamboyant beds

--- src/main/java/co/eltrut/flamboyant/core/Flamboyant.java
+++ src/main/java/co/eltrut/flamboyant/core/Flamboyant.java
@@ -1,12 +1,21 @@
 package co.eltrut.flamboyant.core;
 
+import java.util.HashSet;
+import java.util.Map;
+
 import co.eltrut.differentiate.core.registrator.Registrator;
 import co.eltrut.flamboyant.core.other.FlamboyantCompat;
 import co.eltrut.flamboyant.core.registrator.FBlockHelper;
+import co.eltrut.flamboyant.core.registry.FlamboyantBlocks;
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.properties.BedPart;
+import net.minecraft.village.PointOfInterestType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -33,11 +42,27 @@ public class Flamboyant {
     }
 
     private void doCommonStuff(final FMLCommonSetupEvent event) {
+    	event.enqueueWork(() -> {
+    		this.replaceBedPOI();
+    	});
     }
     
     private void doClientStuff(final FMLClientSetupEvent event) {
     	event.enqueueWork(() -> {
     		FlamboyantCompat.registerEntityRenderers();
     	});
+    }
+    
+    private void replaceBedPOI() {
+    	PointOfInterestType.HOME.matchingStates = new HashSet<>(PointOfInterestType.HOME.matchingStates);
+    	Map<BlockState, PointOfInterestType> map = ObfuscationReflectionHelper.getPrivateValue(PointOfInterestType.class, null, "field_221073_u");
+    	FlamboyantBlocks.BEDS.stream().flatMap(s -> {
+    		return s.get().getStateDefinition().getPossibleStates().stream();
+    	}).filter(s -> {
+    		return s.getValue(BedBlock.PART) == BedPart.HEAD;
+    	}).forEach(s -> {
+    		map.put(s, PointOfInterestType.HOME);
+    		PointOfInterestType.HOME.matchingStates.add(s);
+    	});;
     }
 }
